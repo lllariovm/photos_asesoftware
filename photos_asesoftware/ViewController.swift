60308e0c477da60b240be5890c75ef8ce22c3a2a
Mario Valencia <mvalencia@crnova.com>
2024-02-14 14:20:16 -0500

    Initial Commit

---
+++ photos_asesoftware/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  photos_asesoftware
+//
+//  Created by Mario Valencia Morales on 14/02/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
